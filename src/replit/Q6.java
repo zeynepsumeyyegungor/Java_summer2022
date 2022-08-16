34e72fdb00970e4abd390917b563a14e707d62c5
Zeynep Sumeyye Gungor <gungorsumeyye3520@gmail.com>
2022-08-16 13:15:05 +0300

    16/08/22

---
+++ src/replit/Q6.java
@@ -0,0 +1,13 @@
+package replit;
+
+public class Q6 {
+    public static void main(String[] args) {
+        int s1=22;
+        int s2=20;
+        int s3=30;
+        int s4=28;
+        int s5=50;
+
+        System.out.println("Sayilarin Ortalamasi = " + ((s1+s2+s3+s4+s5)/5));
+    }
+}
